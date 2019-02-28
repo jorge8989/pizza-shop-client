0a18b69dccb51fcd5c312578517e7f0204f8d347
jorge8989 <jorgearteaga8936@gmail.com>
2019-02-28 04:00:54 -0400

    added page por adding and listing orders

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,20 +1,76 @@
 var vueRef;
 function OrdersClient() {
   function init() {
-    console.log('init');
+    Vue.component('products-table-component', {
+      template: $('.products-table-template').text(),
+      props: ['products']
+    });
     var OrdersTableComponent = Vue.extend({
       template: $('.orders-table-template').text(),
       props: ['orders']
+    });
+    var OrderFormComponent = Vue.extend({
+      template: $('.order-form-template').text(),
+      props: ['orders'],
+      data: function() {
+        return {
+          cheeses: ['Cheddar', 'American'],
+          sauces: ['Tomato', 'Pomodoro'],
+          sizes: ['Small', 'Medium', 'Big'],
+          crusts: ['Thin', 'Thick', 'Filled with cheese'],
+          slices_options: [8, 10, 12, 14, 16],
+          order: {
+            state: 'queued'
+          },
+          products: [],
+          product: {
+            food_id: 1,
+            flavor_id: 1,
+            food_name: "Pizza",
+            sauce: 'Tomato',
+            cheese: 'Cheddar',
+            crust: 'Thick',
+            size: 'Small',
+            slices: 8,
+            toppings: []
+          }
+        }
+      },
+      methods: {
+        submit: function() {
+          this.products.push(Object.assign({}, this.product));
+        },
+        makerOrder: function() {
+          var body = {
+            products_attributes: this.products
+          }
+          var self = this;
+          $.ajax({
+            type: "POST",
+            url: 'http://localhost:3010/api/orders',
+            data: body,
+            dataType: 'JSON',
+            success: function(data) {
+              self.orders.unshift(data);
+            }
+          });
+        }
+      }
     });
 
     vueRef = new Vue({
       el: '.vue-app',
       components: {
-        'orders-table-component': OrdersTableComponent
+        'orders-table-component': OrdersTableComponent,
+        'order-form-component': OrderFormComponent
       },
       data: function() {
+        var order = {
+          state: 'queued'
+        };
         return {
-          orders: [{id: 1, state: 'q'}, {id: 2, state: 'q'}]
+          orders: [{id: 1, state: 'q'}, {id: 2, state: 'q'}],
+          order: order
         };
       },
       methods: {
@@ -25,10 +81,12 @@ function OrdersClient() {
             url: 'http://localhost:3010/api/orders',
             dataType: 'JSON',
             success: function(data) {
-              console.log(data);
               self.orders = data;
             },
           });
+        },
+        addorder: function(order) {
+          this.orders.unshift(order);
         }
       },
       mounted: function() {
@@ -39,6 +97,5 @@ function OrdersClient() {
   init();
 }
 $( window ).on( "load", function() {
-  console.log('load');
   new OrdersClient();
 });
